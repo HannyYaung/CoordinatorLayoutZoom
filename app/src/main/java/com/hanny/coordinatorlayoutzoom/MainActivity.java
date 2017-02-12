655c134ab515083810ec75c0e1eebb3f147cd735
hannyyaung <root997@163.com>
2017-02-12 14:21:38 +0700

    Initial commit

---
+++ app/src/main/java/com/hanny/coordinatorlayoutzoom/MainActivity.java
@@ -0,0 +1,58 @@
+package com.hanny.coordinatorlayoutzoom;
+
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CollapsingToolbarLayout toolbarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        setCollapsingToolbarLayoutTitle("约束布局");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_more, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            onBackPressed();
+        } else if (itemId==R.id.action_more){
+            BottomSheetDialog dialog = new BottomSheetDialog(MainActivity.this);
+            View view = getLayoutInflater().inflate(R.layout.reading_actions_sheet, null);
+            dialog.setContentView(view);
+            dialog.show();
+        }
+        return true;
+    }
+
+    private void setCollapsingToolbarLayoutTitle(String title) {
+        toolbarLayout.setTitle(title);
+        toolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+        toolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppBar);
+        toolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBarPlus1);
+        toolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppBarPlus1);
+    }
+}
